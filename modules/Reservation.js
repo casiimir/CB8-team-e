4ef98e650336d639660fd07e370008751b7c697f
Alessandro Dominici <alessandrodomi00@gmail.com>
2024-04-03 10:16:54 +0200

    dbConnect and schemas (user and reservation)

---
+++ modules/Reservation.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const ReservationSchema = new mongoose.Schema({
+  userId: {
+    type: Number,
+    required: true,
+  },
+  eventId: {
+    type: Number,
+    required: true,
+  },
+  ticketsBooked: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports =
+  mongoose.models.ReservationSchema ||
+  mongoose.model("Reservation", ReservationSchema);
---
+++ modules/User.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  businessName: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports =
+  mongoose.models.UserSchema || mongoose.model("User", UserSchema);

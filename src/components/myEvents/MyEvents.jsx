d3cade35f82587a5dd11e0d6ad5dde465c72c860
pepni <pepnicotra90@gmail.com>
2024-04-11 00:12:23 +0200

    pagina eventi creati dall organizzatore e pagina 404

---
+++ src/components/myEvents/MyEvents.jsx
@@ -0,0 +1,22 @@
+import { HTTP_GET } from "../../../libs/HTTP";
+import { useState, useEffect } from "react";
+import EventList from "@/components/eventList";
+
+const MyEvents = () => {
+  const [myEvent, setMyEvents] = useState([]);
+  useEffect(() => {
+    const getMyEvents = async () => {
+      const events = await HTTP_GET(
+        `events/search?organizerId=3 
+        `
+      );
+      setMyEvents(events);
+    };
+    getMyEvents();
+  }, []);
+  return (
+    <EventList title={"i miei eventi"} events={myEvent} endPoint={"events"} />
+  );
+};
+
+export default MyEvents;
---
+++ src/pages/myEvents/index.jsx
@@ -0,0 +1,5 @@
+import MyEvents from "@/components/myEvents";
+
+export default function myEventsList() {
+  return <MyEvents />;
+}
